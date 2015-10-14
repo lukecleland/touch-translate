e30fa57dfbc4a9b2e05fd85cfe5b13cb6b7c11cf
Luke Cleland <Luca@Luke.local>
2015-10-14 12:09:32 +1100

    functioning version

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var app = express();
 var apikey = 'AIzaSyAl2_VXl-axjtU_zGj-s8h1ShY3H3Le_14';
 var googleTranslate = require('google-translate')(apikey);
 
-var server = app.listen(8080, function(){
+var server = app.listen(3000, function(){
     console.log('Server Started');
 });
 
@@ -18,6 +18,7 @@ var io = require('socket.io').listen(server);
 io.on('connection', function(socket){
   console.log('a user connected.. emitting book');
     fs.readFile(__dirname + '/books/starwars.txt', 'utf8', function (err, data) {
+        data = data.replace(/(?:\r\n|\r|\n)/g, ' <br> ');
         var bookarray = data.split(' ');
         for(i=200;i<1200;i++) {
             io.emit('book', bookarray[i]);
